65bbc1a975b45d7e08d45532a471f25c39840c44
Kyuupid <kittipongprasompong@gmail.com>
2024-06-01 01:19:06 +0700

    add more func in math package

--- Go/05_Package/custom/math/math.go
+++ Go/05_Package/custom/math/math.go
@@ -1,9 +1,29 @@
 package math
 
 func Average(i []float64) float64 {
-  total := float64(0)
-  for _, x := range i {
-    total += x
-  }
-  return total / float64(len(i))
+	total := float64(0)
+	for _, x := range i {
+		total += x
+	}
+	return total / float64(len(i))
+}
+
+func Min(i []float64) float64 {
+	min := i[0]
+	for _, x := range i {
+		if x < min {
+			min = x
+		}
+	}
+	return min
+}
+
+func Max(i []float64) float64 {
+	max := i[0]
+	for _, x := range i {
+		if x > max {
+			max = x
+		}
+	}
+	return max
 }
